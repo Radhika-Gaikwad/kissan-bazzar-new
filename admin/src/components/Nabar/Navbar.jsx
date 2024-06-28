08de0bf93b6813331b440c12f10516ebefba3cba
Gaikwad Radhika <139673080+Radhika-Gaikwad@users.noreply.github.com>
2024-06-28 15:38:24 +0530

    Update Navbar.jsx

--- admin/src/components/Nabar/Navbar.jsx
+++ admin/src/components/Nabar/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import './Navbar.css'
-import {assets} from '../../assets/admin_assets/assets'
+import React from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/admin_assets/assets';
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="" />
-      <img className='profile' src={assets.profile_image}alt="" />
+      <a href="https://kissan-bazzar-new-frontend.onrender.com/">
+        <img className='logo' src={assets.logo} alt="Logo" />
+      </a>
+      <img className='profile' src={assets.profile_image} alt="Profile" />
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
