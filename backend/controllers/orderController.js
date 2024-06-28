4d5c192b2cb9125eeed4fd72d8709d684fa45b2d
Gaikwad Radhika <139673080+Radhika-Gaikwad@users.noreply.github.com>
2024-06-28 14:41:53 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://kissan-bazzar-new-frontend.onrender.com/cart";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
